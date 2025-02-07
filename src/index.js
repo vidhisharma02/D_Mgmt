27f579628b74864387bc192b949ba0dabb6848ff
vidhisharma02 <126851410+vidhisharma02@users.noreply.github.com>
2025-02-07 16:02:54 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App"; // Ensure App.js exists in src
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
